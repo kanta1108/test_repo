dc3ddadc36081973fbed7038ca8ae8b3bdf05faa
szkknt <yahooooooooogle@gmail.com>
2022-11-28 14:54:51 +0900

    pug の使用

--- webpack.config.js
+++ webpack.config.js
@@ -27,14 +27,20 @@ module.exports = {
         generator: {
           filename: "images/[name][ext]",
         },
+        use: [],
+      },
+      {
+        test: /\.pug/,
         use: [
-          //   {
-          //     loader: "file-loader",
-          //     options: {
-          //       esModule: false,
-          //       name: "images/[name].[ext]",
-          //     },
-          //   },
+          {
+            loader: "html-loader",
+          },
+          {
+            loader: "pug-html-loader",
+            options: {
+              pretty: true,
+            },
+          },
         ],
       },
     ],
@@ -44,7 +50,12 @@ module.exports = {
       filename: "./stylesheets/main.css",
     }),
     new HtmlWebpackPlugin({
-      template: "./src/templates/index.html",
+      template: "./src/templates/index.pug",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/access.pug",
+      filename: "access.html",
     }),
     new CleanWebpackPlugin(),
   ],
